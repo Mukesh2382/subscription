ac94c6b376817d3d6d310a8cee5b51875b253a82
mukesh <mukesh.choudhari@wisdmlabs.com>
2023-03-20 16:23:10 +0530

    added code to the frontend

--- subs.php
+++ subs.php
@@ -25,40 +25,28 @@
  * Domain Path:       /languages
  */
 
-function create_my_table()
-{
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'email_data';
-    $charset_collate = $wpdb->get_charset_collate();
-
-    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
-		emailid varchar(255) NOT NULL
-	  ) $charset_collate;";
-
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
-}
-
-register_activation_hook(__FILE__, 'create_my_table');
-
-
 function handle_my_form()
 {
     global $wpdb;
+
     if (isset($_POST['email'])) {
-        $table_name = $wpdb->prefix . 'email_data';
         $email = sanitize_text_field($_POST['email']);
         $pattern = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
         if (preg_match($pattern, $email)) {
-            $wpdb->insert(
-                $table_name,
-                array(
-                    'emailid' => $email
-                )
-            );
-        }
-        else{
-            echo "<script>alert('Invalid Email ID');</script>";
+            if (isset($_POST['submit'])) {
+
+                $subs_emails = get_option('subs_emails');
+                if (!$subs_emails) {
+                    $subs_emails = array();
+                }
+                $subs_emails[] = $_POST['email'];
+                update_option('subs_emails', $subs_emails);
+
+                // Display a success message
+                echo '<div class="updated"><p>You are subscribed to Daily Updates!</p></div>';
+            }
+        } else {
+            echo '<div class="error"><p>Invalid Email ID</p></div>';
         }
     }
 }
@@ -66,19 +54,10 @@ add_action('init', 'handle_my_form');
 
 
 
-function my_add_menu_pages()
-{
-    add_menu_page(
-        'Subscription Page',
-        'Subscription Details',
-        'manage_options',
-        'subs',
-        'subscribe_me_callback',
-        'dashicons-email',
-        6
-    );
-}
-add_action('admin_menu', 'my_add_menu_pages');
+
+
+add_action( 'wp_head', 'subscribe_me_callback' );
+
 
 function subscribe_me_callback()
 {
@@ -86,17 +65,13 @@ function subscribe_me_callback()
 
     <!--Add Input fields on Schedule Content Page-->
     <div class="wrap">
-        <h1>Subscribe Me..!!</h1>
-
-
         <form class="myform" method="post">
             <input type="hidden" name="action" value="cc_form">
 
             <label for="email">Email:</label>
             <input type="email" name="email" id="email" /><br />
 
-            <?php submit_button('Subscribe'); ?>
-
+            <input type="submit" value="submit" name="submit" />
         </form>
     </div>
 
